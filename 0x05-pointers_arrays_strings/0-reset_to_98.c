81932f7472ab5ce1d119d409701ac4302d202df4
root-alx <2100808713@std.kyu.ac.ug>
2022-10-27 04:47:08 -0700

    a function that updates the ponter value

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+*Function that updates pointer 
+* *n : pointer
+* return void, basically nothing
+*/
+void reset_to_98(int *n)
+{
+  *n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+/**
+*import fucntion prototypes 
+*from here
+*/
+void reset_to_98(int *n);
+
+#endif
