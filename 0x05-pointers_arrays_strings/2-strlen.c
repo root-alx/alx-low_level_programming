5e9f5a0adbe214e01ff76e7dcf3271280302aa8d
root-alx <2100808713@std.kyu.ac.ug>
2022-10-27 08:44:31 -0700

    $ANSWER

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+*int _strlen-_calculates_the_length_of_string
+*@s:_pointer_to_string_length
+*return_string_length
+*/
+int _strlen(char *s)
+{
+int length = 0;
+while(s[length] != 0)
+   {
+     length++;
+   }
+return(length);
+}
--- 0x05-pointers_arrays_strings/README.md
+++ 0x05-pointers_arrays_strings/README.md
@@ -1,2 +1,3 @@
 a function that takes a pointer to an int as parameter and updates the value it points to to 9
 a function that swaps the values of two integers.
+ a function that returns the length of a string.
